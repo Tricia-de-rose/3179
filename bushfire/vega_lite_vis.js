653aa19d46a3a1093595bd6539cf7f9b3421bfb8
Tricia-de-rose <fder0005@student.monash.edu>
2025-10-01 23:02:05 +1000

    Update vega_lite_vis.js

--- bushfire/vega_lite_vis.js
+++ bushfire/vega_lite_vis.js
@@ -1,5 +1,7 @@
-var vg_1 = "aus_bushfire_dotmap.json";  
+var vg_1 = "bushfire/aus_bushfire_dotmap.json";
 
-vegaEmbed('#map', 'aus_bushfire_dotmap.json').then(function(result) {
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view  
-}).catch(console.error)
+vegaEmbed('#map', vg_1).then(function(result) {
+  // Vega view instance
+}).catch(console.error);
+
+

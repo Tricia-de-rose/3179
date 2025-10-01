659295fe33caf1dcd19a9ab836c5ad407867be10
Tricia-de-rose <fder0005@student.monash.edu>
2025-10-01 21:12:59 +1000

    Data Visualisation 2
    Homework week 9 files

---
+++ bushfire/vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "aus_bushfire_dotmap.json";  
+
+vegaEmbed('#map', 'aus_bushfire_dotmap.json').then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view  
+}).catch(console.error)

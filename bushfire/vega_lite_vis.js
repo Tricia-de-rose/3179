321c68319374824d28f93e976696b9faa6418f03
Tricia-de-rose <fder0005@student.monash.edu>
2025-10-12 22:28:48 +1100

    Update vega_lite_vis.js

--- bushfire/vega_lite_vis.js
+++ bushfire/vega_lite_vis.js
@@ -4,4 +4,16 @@ vegaEmbed('#map', vg_1).then(function(result) {
   // Vega view instance
 }).catch(console.error);
 
+// Temperature Anomaly – Australia Getting Warmer
+var vg_2 = "bushfire/australia_temp_anomaly.json";
+vegaEmbed("#tempanomaly", vg_2).catch(console.error);
+
+// CO₂ Emissions by State – Stacked Bar
+var vg_3 = "bushfire/australia_emissions_by_state.json";
+vegaEmbed("#emissionsbarchart", vg_3).catch(console.error);
+
+// Impact on animals due to the bushires – Donut Chart
+var vg_4 = "bushfire/bushfire_animals_donut.json";
+vegaEmbed("#donut", vg_4).catch(console.error);
+
 
